65a8d9dc81766e6dc092b4773edf4b83e9173f71
Olena Kalitsinska <olena.kalitsinka@gmail.com>
2024-07-30 11:04:15 +0300

    change relative imports to absolute imports and add blank lines between import groups in view.py

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,14 +1,11 @@
-# Standard library imports
 from datetime import datetime, timedelta
 
-# Django imports
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
 
-# Local application imports
 from task_manager.forms import (
     TaskForm,
     WorkerCreationForm,
